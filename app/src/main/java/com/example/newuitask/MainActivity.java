7655acf3fda7e1317fdcdb82a6a264733529e66c
vinodrayka <vinodrayka07@gmail.com>
2024-11-08 17:17:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newuitask/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.newuitask;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioButton radioButtonrupees, radioButtongrams;
+    TextView tvrupees, tvgrams;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        radioButtonrupees = findViewById(R.id.radiorupee);
+        radioButtongrams = findViewById(R.id.radioGram);
+        radioButtonrupees.setActivated(true);
+
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
